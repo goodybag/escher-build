71ad6d2f09f19215e96742bdfdad1f97f9ffefea
John Fawcett <jrf0110@gmail.com>
2013-01-09 14:36:45 -0600

    everything is working

--- index.js
+++ index.js
@@ -2,73 +2,232 @@ var
   fs            = require('fs')
 , sys           = require('sys')
 , childProcess  = require('child_process')
-;
+, async         = require('async')
+, parser        = require('uglify-js').parser
+, uglify        = require('uglify-js').uglify
+, wrench        = require('wrench')
 
-module.exports = function(appPath, callback){
-  var
-    command = "jam compile"
+  // Global options - I promise to fulfill!
+, options
 
-  , getArg = function(option, fallback){
-      var pos = process.argv.indexOf(option);
-      if (pos === -1) return fallback;
-      return process.argv[pos + 1];
-    }
-
-  , onFileRead = function(error, data){
+, getEscherConfig = function(appPath, callback){
+    fs.readFile(appPath, 'utf-8', function(error, data){
       if (error) return callback(error);
 
-      // TODO:
-      // Need to find what variable escher is assigned to
-      var config = data.substring(data.indexOf("escher.config({") + "escher.config(".length);
+      // Parse code into a predictable format
+      data = uglify.gen_code(parser.parse(data, true));
+
+      // Get the variable that escher is assigned to
+      var name = data.substring(0, data.indexOf('=require("escher")')).split(/ |,/g);
+      name = name[name.length - 1];
+
+      // Parse the config
+      var config = data.substring(data.indexOf(name + ".config({") + (name + "config({").length);
       config = config.substring(0, config.indexOf(");"));
-      // It's not valid json so... eval!!
+
+      // Parse the array of apps
       config = eval('(' + config + ')');
 
-      onConfigParsed(config);
-    }
+      return callback(null, config);
+    });
+  }
 
-  , onConfigParsed = function(config){
-      includeRootApp();
-      includeEscherApps(config);
-      includeJamPackages(require(process.cwd() + '/jam/require.config.js'));
+, getJamConfig = function(callback){
+    var config = require(process.cwd() + '/jam/require.config.js');
+    if (callback) return callback(null, config);
+    return config;
+  }
 
-      command += " \\ \n -o " + getArg('-o', 'build.js');
+// Nope
+// , replaceDotJsInDefines = function(file, callback){
+//     fs.readFile(file, 'utf-8', function(error, data){
+//       if (error) return callback(error);
 
-      if (getArg('--show-output', true)) console.log(command);
+//       console.log(data.replace(/define\(.*\.js/g));
+//       callback();
+//     });
+//   }
 
-      if (getArg('--output-only', false)) return;
+, compileMain = function(escherConfig, jamConfig, callback){
+    var
+      command = "jam compile"
+    ;
 
-      execute();
-    }
+    getEscherPackages(escherConfig, function(error, escherPackages){
+      if (error) return callback(error);
 
-  , includeRootApp = function(){
-      command += " \\ \n -i " + appPath;
-    }
+      // Include root app
+      command += " \\ \n -i " + options.appPath;
 
-  , includeEscherApps = function(config){
       // compile command for escher apps
-      for (var i = config.apps.length - 1; i >= 0; i--){
-        command += " \\ \n -i " + config.apps[i] + "/package.js"
+      for (var i = escherConfig.apps.length - 1; i >= 0; i--){
+        command += " \\ \n -i " + escherConfig.apps[i] + "/package"
       }
-    }
 
-  , includeJamPackages = function(config){
+      // Include application routers
+      for (var i = escherPackages.length - 1; i >= 0; i--){
+        command += " \\ \n -i " + escherPackages[i].router;
+      }
+
       // compile command for jam packages
-      for (var i = config.packages.length - 1; i >= 0; i--){
-        command += " \\ \n -i " + config.packages[i].name
-        // command += " \\ \n -i " + config.packages[i].location + "/" + (config.packages[i].main || "main.js")
+      for (var i = jamConfig.packages.length - 1; i >= 0; i--){
+        command += " \\ \n -i " + jamConfig.packages[i].name
       }
-    }
 
-  , execute = function(){
+      // Add the output file
+      command += " \\ \n -o " + options.outputDir + "/" + options.appPath;
+
+      if (!options.optimize) command += " \\ \n --no-minify";
+
+      if (!options.silent) console.log(command);
+
+      // Do some clean up
       command = command.replace(/\\/g, "");
       command = command.replace(/\n/g, "");
       command = command.replace(/   /g, " ");
+
+      // Execute
       childProcess.exec(command, function(error, stdout){
-        sys.puts(stdout);
+        if (error) return callback(error);
+
+        if (!options.silent) sys.puts(stdout);
+
+        return callback();
+      });
+    });
+  }
+
+  // Get an individual package.js file
+, getPackageJs = function(path, callback){
+    fs.readFile(path + '/package.js', 'utf-8', function(error, data){
+      if (error) return callback(error);
+
+      // Parse code into a predictable format
+      data = uglify.gen_code(parser.parse(data, true));
+
+      data = data.substring(data.lastIndexOf('return{') + 'return{'.length - 1);
+      data = data.substring(0, data.length - 2);
+
+      data = eval('(' + data + ')');
+
+      // Put the full path in the package
+      if (!data.path) data.path = './views/app';
+      data.path = data.path.replace('./', path + '/');
+
+      // Do the same for the router
+      if (!data.router) data.router = './router';
+      data.router = data.router.replace('./', path + '/');
+
+      return callback(null, data);
+    });
+  }
+
+  // Get each packages package.js file
+, getEscherPackages = function(config, callback){
+    var
+      packages    = []
+    , numComplete = 0
+    , bail        = false
+    ;
+
+    for (var i = config.apps.length - 1; i >= 0; i--){
+      getPackageJs(config.apps[i], function(error, package){
+        if (bail) return;
+        if (error) return bail = true, callback(error);
+
+        packages.push(package);
+
+        if (++numComplete === config.apps.length) return callback(null, packages);
       });
     }
-  ;
+  }
 
-  fs.readFile(appPath, 'utf-8', onFileRead);
+, compileEscherApp = function(package, callback){
+    var
+      command   = "jam compile"
+    , jamConfig = getJamConfig()
+    ;
+
+    // Include root app
+    command += " \\ \n -i " + package.path;
+
+    // Exclude all jam dependencies
+    for (var i = jamConfig.packages.length - 1; i >= 0; i--){
+      command += " \\ \n -E " + jamConfig.packages[i].name
+    }
+
+    // Exclude requirejs
+    command += " \\ \n -e requireLib";
+
+    // Add the output file
+    command += " \\ \n -o " +  options.outputDir + "/" + package.name + '.js';
+
+    if (!options.optimize) command += " \\ \n --no-minify";
+
+    if (!options.silent) console.log(command);
+
+    // Do some clean up
+    command = command.replace(/\\/g, "");
+    command = command.replace(/\n/g, "");
+    command = command.replace(/   /g, " ");
+
+    // Execute
+    childProcess.exec(command, function(error, stdout){
+      if (error) return callback(error);
+
+      if (!options.silent) sys.puts(stdout);
+
+      return callback();
+    });
+  }
+
+, compileEscherApps = function(config, callback){
+    getEscherPackages(config, function(error, packages){
+      var
+        inParallel = []
+
+      , getCompileFunc = function(package){
+          return function(done){
+            compileEscherApp(package, done);
+          };
+        }
+      ;
+
+      for (var i = packages.length - 1; i >= 0; i--){
+        inParallel.push(getCompileFunc(packages[i]));
+      }
+
+      async.parallel(inParallel, callback);
+    });
+  }
+
+, cleanBuildDirectory = function(callback){
+    wrench.rmdirRecursive(options.outputDir, function(error){
+      if (error) return callback(error);
+
+      fs.mkdir(options.outputDir, callback);
+    });
+  }
+;
+
+module.exports = function(_options, callback){
+  options = _options;
+
+  cleanBuildDirectory(function(error){
+    if (error) return callback(error);
+
+    getEscherConfig(options.appPath, function(error, escherConfig){
+      if (error) return callback(error);
+
+      compileMain(escherConfig, getJamConfig(), function(error){
+        if (error) return callback(error);
+
+        compileEscherApps(escherConfig, function(error){
+          if (error) return callback(error);
+
+          return callback();
+        })
+      });
+    });
+  });
 };
